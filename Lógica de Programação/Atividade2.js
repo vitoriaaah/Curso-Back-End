b54cf324407b88d1be20da5277bbf6829c969037
Vitória Costa <125307942+vitoriaaah@users.noreply.github.com>
2023-03-01 14:59:42 -0300

    Add files via upload

--- Lógica de Programação/Atividade2.js
+++ Lógica de Programação/Atividade2.js
@@ -1,6 +1,11 @@
 function converteridadeemdiasparadias(anos){
 idadeemdias= anos*365;
 return (idadeemdias);
-}
+}do{
 var anos= parseInt(prompt("Informe sua idade: "));
+if (anos <= 0 || !/^[0-9]+$/.test(anos)){
+    alert("Erro. \nTente novamente");
+}
+}while(anos<=0 && anos == "" || !/^[0-9]+$/.test(anos));
 alert(`Idade em anos: ${anos} \nIdade em dias: ${converteridadeemdiasparadias(anos)}dias`);
+
